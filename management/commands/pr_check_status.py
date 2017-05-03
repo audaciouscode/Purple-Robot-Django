78a1be473e6cfad5397c752e18659912d8289976
Chris J. Karr <chris@audacious-software.com>
2017-05-03 21:01:14 +0000

    Cleaning unused import in pr_check_status management command.

--- management/commands/pr_check_status.py
+++ management/commands/pr_check_status.py
@@ -5,7 +5,6 @@ import datetime
 
 from django.core.management import call_command, get_commands
 from django.core.management.base import BaseCommand
-from django.conf import settings
 from django.db.models import Q
 from django.utils import timezone
 
